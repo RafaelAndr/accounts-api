3a9309a511175bf75b7e1145e95d162b28174777
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-10 17:39:03 -0300

    feat(general): initial project structure

---
+++ src/main/java/br/com/coderbank/financialtransferportal/FinancialtransferportalApplication.java
@@ -0,0 +1,13 @@
+package br.com.coderbank.financialtransferportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialtransferportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialtransferportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/coderbank/financialtransferportal/FinancialtransferportalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coderbank.financialtransferportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialtransferportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
