bc7340f3e2913d3b04cc4606466852a71eeb7c88
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-11 14:46:48 -0300

    feat(transaction): create transaction entity

--- src/main/java/br/com/coderbank/financialtransferportal/dto/response/TransactionResponseDto.java
+++ src/main/java/br/com/coderbank/financialtransferportal/dto/response/TransactionResponseDto.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public record TransactionResponseDto(
-        UUID transactionId,
+        UUID id,
         TransactionType type,
         BigDecimal amount,
         LocalDate dateTime,
